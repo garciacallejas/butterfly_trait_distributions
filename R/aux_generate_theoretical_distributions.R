1c3dbc6826dcb42a846d17d3998595117c24b140
garciacallejas <david.garcia.callejas@gmail.com>
2026-03-25 08:39:50 +0100

    update new figures: map and distributions

---
+++ R/aux_generate_theoretical_distributions.R
@@ -0,0 +1,128 @@
+
+# generate probability density functions with varying moments
+
+library(tidyverse)
+library(moments)
+library(patchwork)
+# -------------------------------------------------------------------------
+
+mean.value <- 3
+sd.value <- 1.2
+variance.value <- sd.value^2
+
+num.observations <- 100000
+
+# idea: a convenient skewed distribution to work with is the gamma. 
+# It has two parameters, shape and scale. 
+# The mean is shape*scale and the variance is shape*scale*scale. 
+# So to match the mean and variance, 
+# set the scale of the gamma equal to the ratio of the variance to the mean. 
+# Then once I have the scale, set the shape to the mean divided by the scale.
+
+gamma.scale <- variance.value/mean.value
+gamma.shape <- mean.value/gamma.scale
+
+# another distribution would be a gaussian with same mean and wider sd
+sd.value.wide <- 2
+
+# yet another, a gamma with different scale and/or shape parameters
+gamma.shape.skewed <- gamma.shape - 3
+gamma.mean.skewed <- gamma.shape.skewed*gamma.scale
+
+# yet another, a gamma with high kurtosis. In this case, sd needs to be larger
+# to accomodate kurtosis > 5
+gamma.shape.kurt <- (mean.value / 1.75)^2
+gamma.scale.kurt <- 1.75 / sqrt(gamma.shape.kurt)
+
+# -------------------------------------------------------------------------
+gaussian.1 <- rnorm(num.observations,mean = mean.value,sd = sd.value)
+gamma.1 <- rgamma(n = num.observations,shape = gamma.shape,scale = gamma.scale)
+gaussian.2 <- rnorm(num.observations,mean = mean.value,sd = sd.value.wide)
+# gamma.2.orig <- rgamma(n = num.observations,shape = gamma.shape.skewed,scale = gamma.scale)
+gamma.2 <- rgamma(n = num.observations,shape = gamma.shape.kurt,scale = gamma.scale.kurt)
+
+gaussian.1.moments <- data.frame(dist = "gaussian.1", mean = mean(gaussian.1), sd = sd(gaussian.1), skewness = moments::skewness(gaussian.1), kurtosis = moments::kurtosis(gaussian.1))
+gaussian.2.moments <- data.frame(dist = "gaussian.2", mean = mean(gaussian.2), sd = sd(gaussian.2), skewness = moments::skewness(gaussian.2), kurtosis = moments::kurtosis(gaussian.2))
+gamma.1.moments <- data.frame(dist = "gamma.1", mean = mean(gamma.1), sd = sd(gamma.1), skewness = moments::skewness(gamma.1), kurtosis = moments::kurtosis(gamma.1))
+gamma.2.moments <- data.frame(dist = "gamma.2", mean = mean(gamma.2), sd = sd(gamma.2), skewness = moments::skewness(gamma.2), kurtosis = moments::kurtosis(gamma.2))
+
+dist.moments <- bind_rows(gaussian.1.moments,gaussian.2.moments,gamma.1.moments, gamma.2.moments)
+
+dist.data <- data.frame(g1 = gaussian.1, g2 = gaussian.2, gamma1 = gamma.1, gamma2 = gamma.2)
+x.axis.lims <- c(-2,9)
+y.axis.lims <- c(0,0.36)
+# -------------------------------------------------------------------------
+
+g1.plot <- ggplot(dist.data, aes(x = g1)) + 
+  geom_density() + 
+  geom_vline(aes(xintercept = dist.moments$mean[1]), linetype = "dashed", color = "darkgreen") + 
+  # scale_y_continuous(breaks = NULL) +
+  theme_bw() + 
+  labs(x = "trait value", y = "density") + 
+  # xlim(x.axis.lims) +
+  scale_x_continuous(limits = x.axis.lims, breaks = c(-2,0,2,4,6,8)) +
+  scale_y_continuous(limits = y.axis.lims, breaks = c(0,0.1,0.2,0.3)) +
+  theme(panel.grid.minor=element_blank()) +
+  # theme(axis.ticks.y = element_blank()) +
+  NULL
+# g1.plot
+
+g2.plot <- ggplot(dist.data, aes(x = g2)) + 
+  geom_density() + 
+  geom_vline(aes(xintercept = dist.moments$mean[2]), linetype = "dashed", color = "darkgreen") + 
+  # scale_y_continuous(breaks = NULL) +
+  theme_bw() + 
+  labs(x = "", y = "") +   
+  # xlim(x.axis.lims) +
+  scale_x_continuous(limits = x.axis.lims, breaks = c(-2,0,2,4,6,8)) +
+  scale_y_continuous(limits = y.axis.lims, breaks = c(0,0.1,0.2,0.3)) +
+  theme(panel.grid.minor=element_blank()) +
+  # theme(axis.ticks.y = element_blank()) +
+  NULL
+# g2.plot
+
+gamma1.plot <- ggplot(dist.data, aes(x = gamma1)) + 
+  geom_density() + 
+  geom_vline(aes(xintercept = dist.moments$mean[3]), linetype = "dashed", color = "darkgreen") + 
+  # scale_y_continuous(breaks = NULL) +
+  theme_bw() + 
+  labs(x = "", y = "") + 
+  scale_x_continuous(limits = x.axis.lims, breaks = c(-2,0,2,4,6,8)) +
+  scale_y_continuous(limits = y.axis.lims, breaks = c(0,0.1,0.2,0.3)) +
+  theme(panel.grid.minor=element_blank()) +
+  # xlim(x.axis.lims) +
+  # theme(axis.ticks.y = element_blank()) +
+  NULL
+# gamma1.plot
+
+gamma2.plot <- ggplot(dist.data, aes(x = gamma2)) + 
+  geom_density() + 
+  geom_vline(aes(xintercept = dist.moments$mean[4]), linetype = "dashed", color = "darkgreen") + 
+  # scale_y_continuous(breaks = NULL) +
+  theme_bw() + 
+  labs(x = "trait value", y = "") +  
+  scale_x_continuous(limits = x.axis.lims, breaks = c(-2,0,2,4,6,8)) +
+  scale_y_continuous(limits = y.axis.lims, breaks = c(0,0.1,0.2,0.3)) +
+  theme(panel.grid.minor=element_blank()) +
+  # xlim(x.axis.lims) +
+  # theme(axis.ticks.y = element_blank()) +
+  NULL
+# gamma2.plot
+
+# -------------------------------------------------------------------------
+
+# dist.plot <- g1.plot + (g2.plot/gamma1.plot/gamma2.plot)
+
+layout <- "
+##BB
+AACC
+##DD
+"
+dist.plot <- g1.plot + g2.plot + gamma1.plot + gamma2.plot + plot_layout(design = layout) 
+
+# dist.plot
+# -------------------------------------------------------------------------
+
+# ggsave("results/images/theoretical_distributions_raw.pdf",dist.plot,
+#        device = cairo_pdf,
+#        width = 8, height = 5,dpi = 300)
---
+++ R/aux_map_sampling_sites.R
@@ -0,0 +1,251 @@
+# this script plots a map of the study sites
+
+# INPUTS
+# - CBMS transects and visits (all_visits.txt, transectos_CBMS_filtrados.txt)
+# - UBMS transects and visits (all_visits.txt, ubms_sites.csv)
+# - land cover raster (ESA_WorldCover_10m_2021_V200_N39E000_Map.tif)
+# - built surface raster (GHS_BUILT_S_E2020_GLOBE_R2023A_4326_3ss_V1_0_R5_C19.tif)
+
+# OUTPUTS
+# - figure (bcn_urb_map)
+
+# NOTE: this code is adapted from a script by Pau Colom. It can be tweaked
+# to plot more complex figures, e.g. with land uses or built-up surface in the background
+# but these are not needed here, as the objective is to show the spatial arrangement of
+# sampling points
+
+# -------------------------------------------------------------------------
+# Clean env and load packages ---
+library(tidyverse)
+library(sf)
+library(raster)
+library(osmdata)
+library(viridis)
+library(ggspatial)
+library(terra)
+
+# -------------------------------------------------------------------------
+# Load data ---
+
+# NOTE: this script will not work out of the box as it requires data that 
+# is not uploaded to ZENODO . See the README for details on files uploaded and permissions
+
+# The path to the data used is set here:
+data_path <- "~/trabajo/projects/BCN/butterfly_community_traits/"
+
+# rasters
+lc_raster <- rast(paste0(data_path,"data/map_data/ESA_WorldCover_10m_2021_V200_N39E000_Map.tif"))
+ghsl <- raster(paste0(data_path,"data/map_data/GHS_BUILT_S_E2020_GLOBE_R2023A_4326_3ss_V1_0_R5_C19.tif"))
+
+# sites
+all_sites <- read.csv2(paste0(data_path,"data/all_visits.txt")) %>% dplyr::select(SITE_ID,SCHEME) %>% 
+  unique
+
+cbms_coords <- read.csv2(paste0(data_path,"data/transectos_CBMS_filtrados.csv"),sep=",") %>%
+  dplyr::select(SITE_ID,lat,lng)
+names(cbms_coords) <- c("SITE_ID","latitude","longitude")
+cbms_coords$SITE_ID <- as.character(cbms_coords$SITE_ID)
+
+ubms_coords <- read.csv2(paste0(data_path,"data/ubms_sites.csv")) %>%
+  dplyr::select(transect_id, transect_latitude, transect_longitude)
+names(ubms_coords) <- c("SITE_ID","latitude","longitude")
+ubms_coords <- ubms_coords[grepl("_A", ubms_coords$SITE_ID),]
+ubms_coords$SITE_ID <- substr(ubms_coords$SITE_ID, 1,nchar(ubms_coords$SITE_ID)-2)
+
+cbms_sites <- as_tibble(all_sites) %>%
+  left_join(cbms_coords) %>% drop_na()
+ubms_sites <- as_tibble(all_sites) %>%
+  left_join(ubms_coords) %>% drop_na()
+
+all_sites <- bind_rows(cbms_sites,ubms_sites)
+
+# --- Convert to sf and merge ---
+# some of this is likely repetitive as it comes from reused code
+
+ubms_sf <- st_as_sf(ubms_sites,
+                    coords = c("longitude", "latitude"),
+                    crs = 4326, remove = FALSE)
+
+ubms_df <- ubms_sf %>%
+  st_drop_geometry() %>%
+  mutate(source = "uBMS") %>%
+  dplyr::select(SITE_ID, longitude, latitude, source)
+
+cbms_df <- cbms_sites %>%
+  mutate(source = "CBMS") %>%
+  dplyr::select(SITE_ID, longitude, latitude, source)
+
+merged_transects <- bind_rows(cbms_df, ubms_df)
+
+m_coord_clean <- merged_transects %>%
+  filter(!is.na(longitude), !is.na(latitude))
+
+m_coord_sf_wgs <- st_as_sf(m_coord_clean,
+                       coords = c("longitude", "latitude"),
+                       crs = 4326, remove = FALSE)
+
+# --- Get Barcelona boundary ---
+
+bcn_boundary <- opq("Barcelona") %>%
+  add_osm_feature(key = "name", value = "Barcelona") %>%
+  osmdata_sf() %>%
+  {.$osm_multipolygons} %>%
+  .[1, ]
+
+# --- Extract built-up values + inside/outside classification ---
+bcn_sites <- st_transform(m_coord_sf_wgs, crs = crs(ghsl))
+bcn_sites$urban_value <- raster::extract(ghsl, bcn_sites)
+
+bcn_sites$context <- ifelse(
+  as.logical(st_within(bcn_sites, bcn_boundary, sparse = FALSE)[,1]),
+  "inside", "outside"
+)
+bcn_sites$context <- factor(bcn_sites$context, levels = c("inside","outside"))
+
+# --- Extract urban values for sites ---
+bcn_sites <- st_transform(m_coord_sf_wgs, crs = crs(ghsl))
+bcn_sites$urban_value <- raster::extract(ghsl, bcn_sites)
+
+# --- Buffer around Barcelona boundary ---
+bcn_boundary_utm <- st_transform(bcn_boundary, 25831) # ETRS89 / UTM zone 31N per Catalunya
+bcn_buffer <- st_buffer(bcn_boundary_utm, dist = 60000) # 60 km buffer
+bcn_buffer_wgs <- st_transform(bcn_buffer, crs = crs(ghsl))
+bcn_buffer_sp <- as_Spatial(bcn_buffer_wgs)
+
+# --- Crop/mask raster ---
+ghsl_crop <- crop(ghsl, bcn_buffer_sp)
+ghsl_mask <- mask(ghsl_crop, bcn_buffer_sp)
+
+ghsl_df_bcn <- as.data.frame(rasterToPoints(ghsl_mask))
+names(ghsl_df_bcn)[3] <- "layer"
+ghsl_df_bcn$log_layer <- log1p(ghsl_df_bcn$layer)
+
+# --- Inside vs outside classification ---
+inside_mat <- st_within(bcn_sites, bcn_boundary, sparse = FALSE)
+inside_flag <- as.logical(inside_mat[,1])
+
+# bcn_sites$context <- ifelse(inside_flag, "inside", "outside")
+# bcn_sites$context <- factor(bcn_sites$context, levels = c("inside", "outside"))
+
+bcn_sites$context <- ifelse(inside_flag, "filtered", "regional")
+bcn_sites$context <- factor(bcn_sites$context, levels = c("filtered", "regional"))
+
+# --- Filter sites within buffer ---
+bcn_sites$in_buffer <- as.logical(st_within(bcn_sites, st_as_sf(bcn_buffer_wgs), sparse = FALSE)[,1])
+sites_buffer_bcn <- bcn_sites[bcn_sites$in_buffer, ]
+
+# --- Reprojectar transectes al CRS del raster ---
+bcn_sites <- st_transform(m_coord_sf_wgs, crs = crs(lc_raster))
+
+# Extreure valors land cover per cada punt
+bcn_sites$lc_value <- terra::extract(lc_raster, vect(bcn_sites))[,2]
+
+# --- Crop i mask al buffer ---
+lc_crop <- terra::crop(lc_raster, vect(bcn_buffer_wgs))
+lc_mask <- terra::mask(lc_crop, vect(bcn_buffer_wgs))
+
+lc_mask_filled <- lc_mask
+lc_mask_filled[is.na(lc_mask_filled)] <- 80
+
+# --- Reduir resolució per fer-ho més lleuger (250 m) ---
+lc_raster_low <- aggregate(lc_mask_filled, fact = 25, fun = modal)
+lc_df_bcn <- as.data.frame(lc_raster_low, xy = TRUE)
+names(lc_df_bcn)[3] <- "class"
+
+lc_df_bcn$class <- factor(
+  lc_df_bcn$class,
+  levels = c("50", "10", "20", "30", "60", "40", "90", "80")  # ordre desitjat
+)
+
+water_df_bcn <- lc_df_bcn %>% # Run BCN land cover map
+  dplyr::filter(class == 80)
+
+# -------------------------------------------------------------------------
+# Plot
+
+map_urb_bcn <- ggplot() +
+  # geom_raster(data = ghsl_df_bcn, aes(x = x, y = y, fill = log_layer), alpha = 0.3) +
+  geom_raster(data = ghsl_df_bcn, aes(x = x, y = y), fill = "darkolivegreen3", alpha = 0.3) +
+  geom_sf(data = bcn_boundary, fill = NA, color = "grey30", size = 1) +
+  geom_raster(data = water_df_bcn, aes(x = x, y = y), fill = "white", alpha = 1) +  # <-- water layer
+  geom_sf(data = bcn_buffer_wgs, fill = NA, size = .7) +
+  geom_sf(data = sites_buffer_bcn, aes(color = context), size = 1.5, alpha = 0.9) +
+  scale_fill_viridis(option = "D", na.value = NA, name = "Built-up surface") +
+  scale_color_manual(name = "Community", values = c("filtered" = "darkred", "regional" = "black")) +
+  scale_x_continuous(
+    breaks = seq(1.5, 3, by = 0.5)  
+  ) +
+  scale_y_continuous(
+    breaks = seq(41, 42, by = 0.5),limits = c(41.18,42)
+  )+
+  labs(x = "Longitude", y = "Latitude") +
+  theme_minimal(base_family = "Garamond", base_size = 18) +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.8),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    axis.text = element_text(family = "Garamond", size = 14),
+    axis.title = element_text(family = "Garamond", size = 18),
+    legend.position=c(.15, .9),
+    legend.text = element_text(family = "Garamond", size = 14),
+    legend.title = element_text(family = "Garamond", size = 16)
+  ) +
+  guides(color = guide_legend(override.aes = list(size = 5))) +
+  annotation_scale(
+    location = "tr", 
+    width_hint = 0.25,
+    text_family = "Garamond",  
+    text_cex = 1            
+  )
+
+# map_urb_bcn
+
+# -------------------------------------------------------------------------
+# zoom over the city
+map_city <- ggplot() +
+  geom_sf(data = bcn_boundary, fill = NA, color = "grey30", size = 1) +
+  # geom_raster(data = water_df_bcn, aes(x = x, y = y), fill = "white", alpha = 1) +  # <-- water layer
+  # geom_sf(data = bcn_buffer_wgs, fill = NA, size = .7) +
+  geom_sf(data = subset(sites_buffer_bcn, context == "filtered"), color = "darkred", size = 1.5, alpha = 0.9) +
+  lims(x = c(2.05,2.3), y = c(41.3,41.5)) +
+  # scale_x_continuous(
+  #   breaks = seq(1.5, 3, by = 0.5)  
+  # ) +
+  # scale_y_continuous(
+  #   breaks = seq(41, 42, by = 0.5),limits = c(41.18,42)
+  # )+
+  # labs(x = "Longitude", y = "Latitude") +
+  theme_minimal(base_family = "Garamond", base_size = 18) +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.8),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    axis.text = element_text(family = "Garamond", size = 14),
+    axis.title = element_text(family = "Garamond", size = 18),
+    legend.position=c(.15, .9),
+    legend.text = element_text(family = "Garamond", size = 14),
+    legend.title = element_text(family = "Garamond", size = 16)
+  ) +
+  guides(color = guide_legend(override.aes = list(size = 5))) +
+  # annotation_scale(
+  #   location = "tr", 
+  #   width_hint = 0.25,
+  #   text_family = "Garamond",  
+  #   text_cex = 1            
+  # ) + 
+  NULL
+
+map_city
+# -------------------------------------------------------------------------
+# store
+
+# ggsave("results/images/bcn_urb_map.pdf",
+#        map_urb_bcn,
+#        device = cairo_pdf,
+#        width = 8,
+#        height = 6,
+#        dpi = 300,
+#        bg = "white")
+
